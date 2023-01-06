873b25063551d4b5d9615928792b127f3e44745a
stephenHahn88 <89817088+stephenHahn88@users.noreply.github.com>
2023-01-06 10:38:29 -0500

    added linux json path

--- schenkComposer/config.py
+++ schenkComposer/config.py
@@ -1,1 +1,2 @@
-PRODUCTIONS_JSON_PATH = "C:/Users/88ste/OneDrive/Documents/GitHub/schenkComposer/schenkComposer/data/contours.json"
+# PRODUCTIONS_JSON_PATH = "C:/Users/88ste/OneDrive/Documents/GitHub/schenkComposer/schenkComposer/data/contours.json"
+PRODUCTIONS_JSON_PATH = "/winhomes/sh597/test/schenkComposer/schenkComposer/data/contours.json"
